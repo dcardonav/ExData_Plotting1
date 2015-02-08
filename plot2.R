2fb7f7b52c38f4280d9532e9cba0357e1d82b565
David Cardona <dcardon@gmail.com>
2015-02-08 15:47:11 -0500

    Second Plot

--- plot2.R
+++ plot2.R
@@ -7,17 +7,18 @@
 
 
 # Read the data from the text file, na.Strings is used to specify missing value character
-#raw_data <- read.csv(file = "household_power_consumption.txt", header=TRUE, sep=";",colClasses=c("character", "character", rep("numeric", 7)), na.strings="?")
+raw_data <- read.csv(file = "household_power_consumption.txt", header=TRUE, sep=";",colClasses=c("character", "character", rep("numeric", 7)), na.strings="?")
 
 # Convert first and second column to date and subset data from 2007-02-01 to 2007-02-02
-#dates <- as.POSIXlt(paste(raw_data$Date, raw_data$Time), format="%d/%m/%Y %H:%M:%S")
-#start_date <- as.POSIXlt.character("2007-02-01 00:00:00", format="%Y-%m-%d %H:%M:%S")
-#end_date <- as.POSIXlt.character("2007-02-02 23:59:59", format="%Y-%m-%d %H:%M:%S")
-#ind <- dates >= start_date & dates <= end_date
-#energy <- cbind(dates[ind], raw_data[ind,3:9], row.names=NULL)
+dates <- as.POSIXlt(paste(raw_data$Date, raw_data$Time), format="%d/%m/%Y %H:%M:%S")
+start_date <- as.POSIXlt.character("2007-02-01 00:00:00", format="%Y-%m-%d %H:%M:%S")
+end_date <- as.POSIXlt.character("2007-02-02 23:59:59", format="%Y-%m-%d %H:%M:%S")
+ind <- dates >= start_date & dates <= end_date
+energy <- cbind(dates[ind], raw_data[ind,3:9], row.names=NULL)
 
 # delete original data to save memory space
-#rm(raw_data)
+rm(raw_data)
+rm(ind)
 
 # Start PNG graphics device according to assigment requirements
 png(filename="plot2.png", width=480, height=480)
